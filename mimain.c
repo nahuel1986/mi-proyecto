d8a6e4e8e9c720d97083a08cdf871527db1fe5b8
nahuel1986 <ncalandra@frh.utn.edu.ar>
2026-04-26 16:44:46 -0300

    Update mimain.c

--- mimain.c
+++ mimain.c
@@ -7,4 +7,5 @@ printf("hola estoy en mi main");
 printf("modifico el programa");
 printf("otra modificacion");
 printf("nueva modificacion del main");
+printf("modificacion realizada desde el github");
 }
