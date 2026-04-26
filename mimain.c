2f904c81d043b382dedf8c9f6e4ccc4020954326
nahuel1986 <ncalandra@frh.utn.edu.ar>
2026-04-26 16:51:40 -0300

    Update mimain.c

--- mimain.c
+++ mimain.c
@@ -8,4 +8,5 @@ printf("modifico el programa");
 printf("otra modificacion");
 printf("nueva modificacion del main");
 printf("modificacion realizada desde el github");
+printf("cambio para ver el git fetch");
 }
