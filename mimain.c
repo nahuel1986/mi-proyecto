4af48290270026ea90e570272e6b827a2db6c8e3
NAHUEL CALANDRA <nahuel1986.nc@gmail.com>
2026-04-28 19:49:51 -0300

    modifico desde mi git

--- mimain.c
+++ mimain.c
@@ -7,4 +7,6 @@ printf("hola estoy en mi main");
 printf("modifico el programa");
 printf("otra modificacion");
 printf("nueva modificacion del main");
+
+printf("modifico el main para subirlo a mi github");
 }
